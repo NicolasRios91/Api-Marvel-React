cb8dbbc5dc2c5db85e917f72813e4ed9e6f22892
unknown <nicolasrios91@hotmail.com>
2021-06-09 18:30:12 -0300

    feat:added banner image,fix size of cards and name position

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardList = ({ characterList }) => {
   return (
     <div className="App-Card-List">
-      {characterList?.map((element) => {
+      {characterList.map((element) => {
         let img = element.thumbnail.path + "." + element.thumbnail.extension;
         return <Card key={element.name} name={element.name} img={img}></Card>;
       })}
