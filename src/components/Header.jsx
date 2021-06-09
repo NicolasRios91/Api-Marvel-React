978550149a35aaaee282b1d79549bcf5b3b90e18
unknown <nicolasrios91@hotmail.com>
2021-06-09 18:30:12 -0300

    feat:added banner image,fix size of cards and name position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Header = ({ onChange }) => {
+const Header = ({ onChange, img }) => {
   return (
     <header className="App-Header">
-      <img src="" alt="" />
-      <input type="text" onChange={(e) => onChange(e.target.value)}></input>
+      <img src={img} alt="banner" />
+      <input
+        type="text"
+        onChange={(e) => onChange(e.target.value)}
+        id="App-Header-Search"
+        placeholder="Search"
+        results="0"
+      ></input>
     </header>
   );
 };
