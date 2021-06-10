f27efe1994d0b5259fb7ff6b09f3978abbb73e8a
unknown <nicolasrios91@hotmail.com>
2021-06-10 18:28:47 -0300

    feat: added modal window, modal style and list of comics

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,9 @@ export function fetchList(searchValue) {
     `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchValue}&ts=1&apikey=3e8159f9102a91676ab9339a3b0e7135&hash=6126f0add6159a284067d294bbd98c92&limit=100`
   );
 }
+
+export function fetchComicList(characterId) {
+  return fetch(
+    `https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?&ts=1&apikey=3e8159f9102a91676ab9339a3b0e7135&hash=6126f0add6159a284067d294bbd98c92&limit=10`
+  );
+}
