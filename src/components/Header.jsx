e54f70792d5b9b3c5697ad7928c26f70a6b2a033
unknown <nicolasrios91@hotmail.com>
2021-06-09 23:34:56 -0300

    feat: added banner parameter and placeholder on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Header = ({ onChange }) => {
+const Header = ({ onChange, img }) => {
   return (
     <header className="App-Header">
-      <img src="" alt="" />
-      <input type="text" onChange={(e) => onChange(e.target.value)}></input>
+      <img src={img} alt="banner" />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => onChange(e.target.value)}
+      ></input>
     </header>
   );
 };
