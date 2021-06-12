4883e266a4c1f1f9df87bae9b40c925169605531
unknown <nicolasrios91@hotmail.com>
2021-06-12 18:58:16 -0300

    feat: added content to Comic preview page

--- src/views/ComicPreview.jsx
+++ src/views/ComicPreview.jsx
@@ -1,13 +1,44 @@
+import "../ComicPreview.css";
 import React from "react";
 import { useLocation } from "react-router";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import marvel from "../img/bannerMarvel.jpg";
 
 const ComicPreview = () => {
   const location = useLocation();
   const comic = location.state.comic;
+  const img = comic.thumbnail.path + "." + comic.thumbnail.extension;
+  const date = comic.dates[0];
+
+  const creators = comic.creators.items;
+  const creatorsValues = creators.map((element) => {
+    let value = {};
+    value["name"] = element["name"];
+    value["role"] = element["role"];
+    return value;
+  });
+
+  let stringCreators = "";
+  creatorsValues.forEach((element) => {
+    stringCreators += `${element.role}: ${element.name} \n`;
+  });
+  const newText = stringCreators.split("\n").map((str) => <p>{str}</p>);
   return (
-    <div>
-      <label htmlFor="">{comic.title}</label>
-    </div>
+    <>
+      <Header img={marvel}></Header>
+      <div className="Comic-Preview-Content">
+        <div className="Comic-Preview-Image">
+          <img src={img} alt="comic Image" id="Comic-Image" />
+        </div>
+        <div className="Comic-Preview-Description">
+          <p>{comic.title}</p>
+          <br />
+          <p>Published: {date.date}</p>
+          <p>{newText}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
