9d99e522c35ba3a654e4ae0fcbe0742abc9019cc
unknown <nicolasrios91@hotmail.com>
2021-06-13 02:38:50 -0300

    feat: added ComicUrl function, comicPreview slayout

--- src/views/ComicPreview.jsx
+++ src/views/ComicPreview.jsx
@@ -1,41 +1,75 @@
 import "../ComicPreview.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import marvel from "../img/bannerMarvel.jpg";
+import { useHistory } from "react-router-dom";
+import { fetchComic } from "../api/index";
 
 const ComicPreview = () => {
   const location = useLocation();
-  const comic = location.state.comic;
-  const img = comic.thumbnail.path + "." + comic.thumbnail.extension;
-  const date = comic.dates[0];
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [comic, setComic] = useState(null);
+  const [image, setImage] = useState();
+  const [date, setDate] = useState();
+  const [creators, setCreators] = useState();
 
-  const creators = comic.creators.items;
-  const creatorsValues = creators.map((element) => {
-    let value = {};
-    value["name"] = element["name"];
-    value["role"] = element["role"];
-    return value;
-  });
+  const formatCreators = (creators) => {
+    let stringCreators = "";
+    creators.forEach((element) => {
+      stringCreators += `${element.role}: ${element.name} \n`;
+    });
+    return stringCreators;
+  };
 
-  let stringCreators = "";
-  creatorsValues.forEach((element) => {
-    stringCreators += `${element.role}: ${element.name} \n`;
-  });
-  const newText = stringCreators.split("\n").map((str) => <p>{str}</p>);
+  const setValues = (comic) => {
+    let stringCreators = formatCreators(comic.creators.items);
+    let formattedCreators = stringCreators
+      .split("\n")
+      .map((str) => <p>{str}</p>);
+    setComic(comic);
+    setImage(comic.thumbnail.path + "." + comic.thumbnail.extension);
+    setDate(comic.dates[0]);
+    setCreators(formattedCreators);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (location.state.comic) {
+      setValues(location.state.comic);
+    } else {
+      fetchComic(location.state.comicId)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((dataResponse) => {
+          setValues(dataResponse.data.results[0]);
+        })
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, []);
+  if (loading) return "Loading..";
+  if (error) return "error..";
+
   return (
     <>
-      <Header img={marvel}></Header>
+      <button onClick={() => history.push("/")}>Back</button>
       <div className="Comic-Preview-Content">
         <div className="Comic-Preview-Image">
-          <img src={img} alt="comic Image" id="Comic-Image" />
+          <img src={image} alt="comic Image" id="Comic-Image" />
         </div>
-        <div className="Comic-Preview-Description">
-          <p>{comic.title}</p>
-          <br />
-          <p>Published: {date.date}</p>
-          <p>{newText}</p>
+        <div className="Comic-Preview-Information">
+          <h3>{comic.title}</h3>
+          <p className="Comic-Preview-data">Published: {date.date}</p>
+          <p className="Comic-Preview-data">{creators}</p>
+          <p className="Comic-Preview-description">{comic.description}</p>
         </div>
       </div>
     </>
